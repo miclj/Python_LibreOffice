1220f628d44fa4f52c9d086e2e68c4f54b2b79ce
miclj <64108986+miclj@users.noreply.github.com>
2020-11-08 18:04:03 +0100

    Add files via upload

--- start.cmd
+++ start.cmd
@@ -9,5 +9,15 @@ for /f "tokens=1,2 delims=#" %%a in ("%str%") do set str=%%a&&set liboInstallPat
 REM On enlève les espaces au début de la partie droite
 for /f "tokens=* delims= " %%a in ("%liboInstallPath%") do set liboInstallPath=%%a
 REM On supprime la variable str
+set args=%1
+shift
+:parse
+set str=%1
+if "%~1"="" goto :fin
+set "args=%args% %str%"
+shift
+goto :parse
+:fin
 set str=
-echo %liboInstallPath%
+%liboInstallPath%\python.exe %args%
+set args=
